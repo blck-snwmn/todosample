7e621305d2c6b312b17fc325adf4faa1ae5debe8
blck-snwmn <whitesnowmancreator@gmail.com>
2022-12-04 16:53:24 +0900

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Todo list'),
     );
   }
 }
